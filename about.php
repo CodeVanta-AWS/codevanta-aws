eb13cfc17068e81656359079a0149e1c189512fc
Laviscyan <laviscyan@gmail.com>
2025-03-23 00:09:11 +0800

    Import Tailwind

--- about.php
+++ about.php
@@ -2,7 +2,13 @@
     include './auth_check.php';
 ?>
 
+<link rel="stylesheet" href="src/assets/styles/output.css">
+
 <main>
+    <div class="">
+        <p class="text-red-500">test</p>
+        
+    </div>
     <?php include("./header.php"); ?>
     <section>about</section>
     <a href="logout.php">
